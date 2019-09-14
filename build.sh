b2fcb6a43eed0746edd0d9309bb7b29866d37dca
Lanode <lanode@mail.ru>
2019-09-14 07:32:34 +0300

    change build file

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #else
 #	lazbuild kbot6.lpi --build-mode=$1;
 #fi
-fpc -MObjFPC -Schi -Cg -O3 -k-R -k./ -l -vewnhibq -Ficompunits/x86_64-linux -Fusrc -Fu. -FUcompunits/x86_64-linux -FEbin -obin/kbot6 -B main.pas 
+fpc -MObjFPC -Schi -Cg -O3 -k-R -k./ -l -vewnhibq -FUcompunits -Ficompunits -Fusrc -Fu. -FEbin -obin/kbot6 -B main.pas 
