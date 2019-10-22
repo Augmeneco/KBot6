e94a21ad5b63502636bb824858300ab99998e4a4
Cha14ka <taitor@yandex.ru>
2019-10-22 19:51:37 +0300

    add "govnokog" plugin

---
+++ bin/plugins/govnokod.py
@@ -0,0 +1,27 @@
+import kb, random, requests, re
+from bs4 import BeautifulSoup as bs
+import libkbot as libkb
+
+class govnokod:
+	level = 1
+	keywords = ['говнокод','govnokod','гв']
+	def handler(self, msg):
+		msg = libkb.fix_names(msg)
+		try:
+			index = requests.get('http://govnokod.ru/'+msg['user_text'].lower()).text
+		except:
+			libkb.apisay('Такого языка не существует :(',msg['toho'])
+			return 0
+		index = bs(index,'html.parser')
+		index = index.find_all('ul','pagination numbered')[0].find_all('a')
+		max = int(index[0].text)+1
+		min = int(index[-1].text)
+		randnum = str(random.randint(min,max))
+		index = requests.get('http://govnokod.ru/'+msg['user_text'].lower()+'?page='+randnum).text
+		index = bs(index,'html.parser')
+		code = re.sub('(\\n\d*\\n)','',random.choice(index.find_all('li','hentry')).find('div').text)
+		description = index.find_all('li','hentry')[0].find('p','description').text
+
+		libkb.apisay(code+'-'*30+description,msg['toho'])
+
+kb.reg_command(govnokod())
